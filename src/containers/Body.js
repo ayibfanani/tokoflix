b38061760f62c24d8e9ac5710ac57408a330bc43
ayibfanani <ayibfanani@gmail.com>
2018-07-15 16:41:31 +0700

    init

--- src/containers/Body.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Body = (props) => (
-  <div>
-    {props.children}
-  </div>
-);
-
-export default Body;
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import {applyMiddleware, createStore} from "redux";
-import thunk from "redux-thunk";
-import {composeWithDevTools} from 'redux-devtools-extension'
-
-import reducers from "./reducers";
-
-const middleware = applyMiddleware(thunk)
-
-export default createStore(reducers, composeWithDevTools(middleware))
